2ead2fca0ba3905d9a6d5c78f4a32779b3434203
William Jackson <lila.lilajackson@hotmail.com>
2024-12-12 12:41:37 -0300

    earning field

--- src/components/renderBill/RenderBill.jsx
+++ src/components/renderBill/RenderBill.jsx
@@ -7,6 +7,7 @@ import './renderBill.css';
 
 const RenderBill = () => {
     const [message, setMessage] = useState([]);
+    const [isPaid, setIsPaid] = useState(false);
 
     useEffect(() => {
         client.subscribe(topic);
@@ -22,39 +23,46 @@ const RenderBill = () => {
     return (
         <div className="wrapper-container-bills-card">
             <div className="wrapper-filters-button">
-                <button onClick={()=> client.publish(`${topic}-filtro-comprador`, 'livia')}>Lívia</button>
-                <button onClick={()=> client.publish(`${topic}-filtro-comprador`, 'william')}>William</button>
-                <button onClick={()=> client.publish(`${topic}-filtro-comprador`, 'miriam')}>Miriam</button>
+                <button onClick={() => client.publish(`${topic}-filtro-comprador`, 'livia')}>Lívia</button>
+                <button onClick={() => client.publish(`${topic}-filtro-comprador`, 'william')}>William</button>
+                <button onClick={() => client.publish(`${topic}-filtro-comprador`, 'miriam')}>Miriam</button>
             </div>
             {
-            message.length > 0 ?
-                message.map((bill, index) => {
-                    return (
-                        <div key={index} className="container-bills-card">
-                            <div className="title-value-div">
-                                <h2>{bill.titulo}</h2>
-                                <p>{`R$${bill.valor}`}</p>
-                            </div>
+                message.length > 0 ?
+                    message.map((bill, index) => {
+                        return (
+                            <div key={index} className="container-bills-card">
+                                <div className="title-value-div">
+                                    <h2>{bill.titulo}</h2>
+                                    <p className="bill-value" onClick={() => {
+                                        client.publish(`${topic}-isPaid`, isPaid.toString());
+                                        setIsPaid(!isPaid);
+                                        console.log('clicou')
+                                    }
+                                    }
+                                    >
+                                        {`R$${bill.valor}`}</p>
+                                </div>
 
-                            <p>{bill.descricao}</p>
-                            <p>{bill.estabelecimento}</p>
-                            <div className="estab-form-pag-div">
-                                <p>{bill.formaDePagamento}</p>
-                                <p>{bill.banco}</p>
+                                <p>{bill.descricao}</p>
+                                <p>{bill.estabelecimento}</p>
+                                <div className="estab-form-pag-div">
+                                    <p>{bill.formaDePagamento}</p>
+                                    <p>{bill.banco}</p>
+                                </div>
+                                <p>{bill.comprador}</p>
+                                <div className="wrapper-categ-trash-icon">
+                                    <p>{bill.categoria}</p>
+                                    <FontAwesomeIcon icon={faTrash} className="trash-icon" onClick={() => {
+                                        client.publish(`${topic}-delete`, `${bill.id}`)
+                                        window.location.reload();
+                                    }} />
+                                </div>
                             </div>
-                            <p>{bill.comprador}</p>
-                            <div className="wrapper-categ-trash-icon">
-                                <p>{bill.categoria}</p>
-                                <FontAwesomeIcon icon={faTrash} className="trash-icon" onClick={() => {
-                                    client.publish(`${topic}-delete`, `${bill.id}`)
-                                    window.location.reload();
-                                }} />
-                            </div>
-                        </div>
-                    )
-                }) :
-                <p>Nada a mostrar</p>
-        }
+                        )
+                    }) :
+                    <p>Nada a mostrar</p>
+            }
         </div>
     )
 };
--- src/components/sumBills/SumBills.jsx
+++ src/components/sumBills/SumBills.jsx
@@ -6,6 +6,7 @@ const SumBills = () => {
     //Faz o fetch no backend e retorna todas as bills
     const [message, setMessage] = useState([]);
     const [totalValueBill, setTotalValueBill] = useState(0);
+    const [totalEarnings, setTotalEarnings] = useState(0);
 
     useEffect(() => {
         client.subscribe(topic);
@@ -18,14 +19,37 @@ const SumBills = () => {
         });
     }, []);
 
-    useEffect(()=> {
+    useEffect(() => {
+        const storedData = localStorage.getItem('earning-input');
+        if (storedData && storedData.length > 0) {
+            setTotalEarnings(storedData);
+        }
+    }, []);
+
+    useEffect(() => {
         const total = message.reduce((sum, bill) => sum + bill.valor, 0);
         setTotalValueBill(total);
-    }, [message]);
+    }, [message]); // Captura o JSON e soma todos os valores
+
+    const saveEarningInput = (e) => {
+        setTotalEarnings(e);
+        localStorage.setItem('earning-input', e);
+    } // Armazena o input digitado
 
     return (
         <div className='container-total-value-bill'>
-            <h2>Total: R$ {totalValueBill.toFixed(2)}</h2>
+            <h2>Gastos: R$ {totalValueBill.toFixed(2)}</h2>
+            <div className="wrapper-gains-input-value">
+                <h3>Ganhos: R$
+                    <input
+                        className='input-earning-value'
+                        type="number"
+                        value={totalEarnings}
+                        content={totalEarnings}
+                        onChange={(e) => saveEarningInput(e.target.value)}
+                    />
+                </h3>
+            </div>
         </div>
     )
 }
