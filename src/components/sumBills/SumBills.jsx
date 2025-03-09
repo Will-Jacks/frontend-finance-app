8a95c278ee94be054fbff55d8f02e0db8bd3ec23
William Jackson <lila.lilajackson@hotmail.com>
2025-03-09 19:55:34 -0300

    removing earning fields

--- src/components/sumBills/SumBills.jsx
+++ src/components/sumBills/SumBills.jsx
@@ -6,7 +6,6 @@ const SumBills = () => {
     //Faz o fetch no backend e retorna todas as bills
     const [message, setMessage] = useState([]);
     const [totalValueBill, setTotalValueBill] = useState(0);
-    const [totalEarnings, setTotalEarnings] = useState(0);
 
     useEffect(() => {
         client.subscribe(topic);
@@ -19,38 +18,14 @@ const SumBills = () => {
     }, []);
 
     useEffect(() => {
-        const storedData = localStorage.getItem('earning-input');
-        if (storedData && storedData.length > 0) {
-            setTotalEarnings(storedData);
-        }
-    }, []);
-
-    useEffect(() => {
         const total = message.reduce((sum, bill) => sum + bill.valor, 0);
         setTotalValueBill(total);
     }, [message]); // Captura o JSON e soma todos os valores
-
-    const saveEarningInput = (e) => {
-        setTotalEarnings(e);
-        localStorage.setItem('earning-input', e);
-    } // Armazena o input digitado
 
     return (
         <div className='container-total-value-bill'>
             <h2>Gastos: R$ {totalValueBill.toFixed(2)}</h2>
             <div className="wrapper-gains-input-value">
-                <h3>Ganhos: R$
-                    <input
-                        className='input-earning-value'
-                        type="number"
-                        value={totalEarnings}
-                        content={totalEarnings}
-                        onChange={(e) => saveEarningInput(e.target.value)}
-                    />
-                </h3>
-            </div>
-            <div>
-                <h3>Restante: ${(totalEarnings - totalValueBill).toFixed(2)}</h3>
             </div>
         </div>
     )
