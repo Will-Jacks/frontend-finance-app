83ff5fbbc85c671bdf08fb62d1ce70c058f3561c
William Jackson <lila.lilajackson@hotmail.com>
2025-05-09 23:07:32 -0300

    adding filter buttons

--- src/components/Filters/BuyerFilter.jsx
+++ src/components/Filters/BuyerFilter.jsx
@@ -4,9 +4,11 @@ import { MQTT_TOPIC } from "../../context/MQTTContext";
 
 //Estilização
 import "./buyerFilter.css";
+import { useState } from "react";
 
 function BuyerFilter({ message, setMessage }) {
     const { client } = useMQTT();
+    const [isActive, setIsActive] = useState(false);
     if (!client) return null;
 
     function handleBuyerFilter(buyer) {
@@ -26,25 +28,21 @@ function BuyerFilter({ message, setMessage }) {
 
     return (
         <div className="wrapper-filter-buttons">
-            <button onClick={() => client.publish(`${MQTT_TOPIC}-parcial-bills`, '.')}>Recentes</button>
-            <button onClick={() => client.publish(`${MQTT_TOPIC}-all`, '.')}>Todos</button>
-            <button onClick={() => client.publish(`${MQTT_TOPIC}-paids`, '.')}>Pagos</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => client.publish(`${MQTT_TOPIC}-parcial-bills`, '.')}>Recentes</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => client.publish(`${MQTT_TOPIC}-all`, '.')}>Todos</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => client.publish(`${MQTT_TOPIC}-paids`, '.')}>Pagos</button>
             <div className="splitter"></div>
-            <button onClick={() => handleBuyerFilter('Lívia')}>Lívia</button>
-            <button onClick={() => handleBuyerFilter('William')}>William</button>
-            <button onClick={() => handleBuyerFilter('Miriam')}>Miriam</button>
-            <button onClick={() => handleBankFilter('Nubank')}>Nubank</button>
-            <button onClick={() => handleBankFilter('Santander')}>Santander</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBuyerFilter('Lívia')}>Lívia</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBuyerFilter('William')}>William</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBuyerFilter('Miriam')}>Miriam</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBankFilter('Nubank')}>Nubank</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBankFilter('Santander')}>Santander</button>
             <div className="splitter"></div>
-            <button
-                onClick={() => handleBankAndBuyerFilter('Santander', 'Lívia')} id="filter-button-santander-livia">Lib</button>
-            <button onClick={() => () => handleBankAndBuyerFilter('Santander', 'William')}
-                id="filter-button-santander-william">Will</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBankAndBuyerFilter('Santander', 'Lívia')} id="filter-button-santander-livia" >Lib</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => () => handleBankAndBuyerFilter('Santander', 'William')} id="filter-button-santander-william">Will</button>
             <div className="splitter"></div>
-            <button onClick={() => handleBankAndBuyerFilter('Nubank', 'Lívia')}
-                id="filter-button-nubank-livia">Lib</button>
-            <button onClick={() => handleBankAndBuyerFilter('Nubank', 'William')}
-                id="filter-button-nubank-william">Will</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBankAndBuyerFilter('Nubank', 'Lívia')} id="filter-button-nubank-livia">Lib</button>
+            <button className={`filter-button ${isActive ? 'active' : ''}`} onClick={() => handleBankAndBuyerFilter('Nubank', 'William')} id="filter-button-nubank-william">Will</button>
         </div>
     )
 }
