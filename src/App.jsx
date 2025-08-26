d78df7d1e1eb4daabfec7b2f02115d7b128b5837
William Jackson <lila.lilajackson@hotmail.com>
2025-08-26 11:11:09 -0300

    fix: applying css modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 //Route
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 //Componentes
@@ -6,17 +5,14 @@ import Home from "./pages/Home";
 import GeneralBills from "./pages/GeneralBills";
 //Utils
 import { MQTTProvider } from "./context/MQTTContext";
-import isTheFirstTime from "./utils/getUsername";
 //Estilização
 import { ToastContainer } from "react-toastify";
 import Months from './pages/Months';
 import NavBar from './components/NavBar/NavBar';
 import UsersInfo from './pages/UsersInfo';
+
 function App() {
 
-  useEffect(() => {
-    isTheFirstTime();
-  }, [])
   return (
     <MQTTProvider>
       <Router>
--- src/pages/UsersInfo.jsx
+++ src/pages/UsersInfo.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-
+import "./userInfo.css";
 function UsersInfo() {
     const [liviaValue, setLiviaValue] = useState(localStorage.getItem("livia-gain-value") || 0);
     const [williamValue, setWilliamValue] = useState(localStorage.getItem("william-gain-value") || 0);
+    const [username, setUsername] = useState(localStorage.getItem("username") || "Fulano");
 
     function handleValue(buyer, e) {
         const newValue = e.target.value;
@@ -18,6 +19,17 @@ function UsersInfo() {
         }
     }
 
+    function handleUsername(e) {
+        const trimmedUsername = (e.target.value).trim();
+        if (trimmedUsername) {
+            const capitalizedUsername = trimmedUsername.charAt(0).toUpperCase() + trimmedUsername.slice(1);
+            setUsername(capitalizedUsername);
+            localStorage.setItem('username', capitalizedUsername);
+        } else {
+            setUsername("");
+        }
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         const actualMonth = (new Date().getMonth() + 1);
@@ -28,34 +40,53 @@ function UsersInfo() {
             ganhosLivia: liviaValue,
             ganhosWilliam: williamValue
         }
-        client.publish(`${MQTT_TOPIC}-get-ganhos-trigger`, JSON.stringify(formattedData));
+        //  client.publish(`${MQTT_TOPIC}-get-ganhos-trigger`, JSON.stringify(formattedData));
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <h2>Renda</h2>
-                <p>Lívia </p><input
-                    type="text"
-                    inputMode="decimal"
-                    pattern="[0-9]*[.,]?[0-9]*"
-                    placeholder="Digite aqui"
-                    value={liviaValue}
-                    onChange={(e) => handleValue("Lívia", e)}
-                    required
-                />
-                <p>William </p><input
-                    type="text"
-                    inputMode="decimal"
-                    pattern="[0-9]*[.,]?[0-9]*"
-                    placeholder="Digite aqui"
-                    value={williamValue}
-                    onChange={(e) => handleValue("William", e)}
-                    required
-                />
-                <button type="submit">Atualizar</button>
-            </form>
-        </>
+        <div className="body">
+            <div className="container">
+                <div className="name-section">
+                    <h2 className="section-title">Nome</h2>
+                    <input
+                        className="input-field"
+                        type="text"
+                        value={username}
+                        onChange={(e) => handleUsername(e)}
+                    />
+                </div>
+                <form className="income-form" onSubmit={handleSubmit}>
+                    <h2 className="section-title">Renda</h2>
+                    <div className="form-group">
+                        <p className="input-label">Lívia</p>
+                        <input
+                            className="input-field"
+                            type="text"
+                            inputMode="decimal"
+                            pattern="[0-9]*[.,]?[0-9]*"
+                            placeholder="Digite aqui"
+                            value={liviaValue}
+                            onChange={(e) => handleValue("Lívia", e)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <p className="input-label">William</p>
+                        <input
+                            className="input-field"
+                            type="text"
+                            inputMode="decimal"
+                            pattern="[0-9]*[.,]?[0-9]*"
+                            placeholder="Digite aqui"
+                            value={williamValue}
+                            onChange={(e) => handleValue("William", e)}
+                            required
+                        />
+                    </div>
+                    <button className="submit-button" type="submit">Atualizar</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
