ce93d7516925d328343051dcf5b29c2f43416633
William Jackson <lila.lilajackson@hotmail.com>
2025-09-04 08:59:44 -0300

    fix: fixing calculate remaining value logic

--- src/components/RemainingValue/RemainingValue.jsx
+++ src/components/RemainingValue/RemainingValue.jsx
@@ -1,25 +1,54 @@
+import useMQTT from "../../hooks/useMQTT";
+import { MQTT_TOPIC } from "../../context/MQTTContext";
 import "./RemainingValue.css";
+import { useEffect, useState } from "react";
 function RemainingValue({ buyersSum }) {
+    const { client } = useMQTT();
+    const [payload, setPayload] = useState();
+    const [willRemainingBalance, setWillRemainingBalance] = useState(0);
+    const [liviaRemainingBalance, setLiviaRemainingBalance] = useState(0);
 
-    function formattingStringLivia() {
-        const localStorageData = localStorage.getItem("livia-gain-value");
-        return Number(localStorageData - buyersSum[0].toFixed(2)) || 0;
+    useEffect(() => {
+        if (!client) { return; }
+
+        function handlePayload(topic, payload) {
+            if (topic == `${MQTT_TOPIC}-income-data-response`) {
+                const parsePayload = JSON.parse(payload.toString());
+                setPayload(parsePayload);
+            };
+        };
+
+        client.subscribe(`${MQTT_TOPIC}-income-data-response`);
+        client.publish(`${MQTT_TOPIC}-get-month-income`, ".");
+        client.on("message", handlePayload);
+
+        return () => {
+            client.off('message', handlePayload);
+            client.unsubscribe(`${MQTT_TOPIC}-income-data-response`);
+        }
+    }, [client]);
+
+    useEffect(() => {
+        calculateRemainingBalance();
+    }, [payload]);
+
+    function calculateRemainingBalance() {
+        if (payload) {
+            setLiviaRemainingBalance(Number(payload.ganhosLivia) - Number(buyersSum[0]));
+            setWillRemainingBalance(Number(payload.ganhosWilliam) - Number(buyersSum[1]));
+        }
     }
 
-    function formattingStringWilliam() {
-        const localStorageData = localStorage.getItem("william-gain-value");
-        return Number(localStorageData - buyersSum[1].toFixed(2)) || 0;
-    }
     return (
         <div className="remaining-balance">
             <h3>Valor restante:</h3>
             <div className="balance-item">
                 <span>Lívia:</span>
-                <span>R$ {formattingStringLivia()}</span>
+                <span>R$ {liviaRemainingBalance}</span>
             </div>
             <div className="balance-item">
                 <span>William:</span>
-                <span>R$ {formattingStringWilliam()}</span>
+                <span>R$ {willRemainingBalance}</span>
             </div>
         </div>
     );
